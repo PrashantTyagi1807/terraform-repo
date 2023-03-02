b4c26d786af0c6053a34497577dde2dba35eb297
Prashant Tyagi <48384710+PrashantTyagi1807@users.noreply.github.com>
2023-03-02 16:59:54 +0530

    Create security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,29 @@
+#Creating a Security Group
+resource "aws_security_group" "SG" {
+  name        = var.sg_tag
+  description = "Allow TLS inbound traffic"
+  vpc_id      = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = var.ports
+    iterator = port
+    content {
+      description = "TLS from VPC"
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.sg_tag
+  }
+}
